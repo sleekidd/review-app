8189a8f803bba2d253cbec5b69d10b177e6f0760
thetobiallen <tobi_newsystem@yahoo.com>
2020-10-22 18:09:39 +0100

    CRUD features added

--- movieapp/urls.py
+++ movieapp/urls.py
@@ -1,7 +1,19 @@
 from django.urls import path
+from .views import (
+    MovieListView,
+    MovieDetailView,
+    MovieCreateView,
+    MovieUpdateView,
+    MovieDeleteView
+)
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='movie-home'),
+    # path('', views.home, name='movie-home'),
+    path('', MovieListView.as_view(), name='movie-home'),
+    path('movie/<int:pk>/', MovieDetailView.as_view(), name='movie-detail'),
+    path('movie/new/', MovieCreateView.as_view(), name='movie-create'),
+    path('movie/<int:pk>/update/', MovieUpdateView.as_view(), name='movie-update'),
+    path('movie/<int:pk>/delete/', MovieDeleteView.as_view(), name='movie-delete'),
     path('about/', views.about, name='movie-about'),
 ]
--- movieapp/views.py
+++ movieapp/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render, HttpResponse
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
 from .models import Movie
 
 # Create your views here.
@@ -9,6 +17,52 @@ def home(request):
         'movies': Movie.objects.all()
     }
     return render(request, 'movieapp/index.html', context)
+
+
+class MovieListView(ListView):
+    model = Movie
+    template_name = 'movieapp/index.html'  # <app>/<model>_<viewtype>/html
+    context_object_name = 'movies'
+    ordering = ['-release_date']
+
+
+class MovieDetailView(DetailView):
+    model = Movie
+
+
+class MovieCreateView(LoginRequiredMixin, CreateView):
+    model = Movie
+    fields = ['title', 'content', 'image', 'cast', 'category', 'release_date']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class MovieUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Movie
+    fields = ['title', 'content', 'image', 'cast', 'category', 'release_date']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        movie = self.get_object()
+        if self.request.user == movie.author:
+            return True
+        return False
+
+
+class MovieDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Movie
+    success_url = '/'
+
+    def test_func(self):
+        movie = self.get_object()
+        if self.request.user == movie.author:
+            return True
+        return False
 
 
 def about(request):
